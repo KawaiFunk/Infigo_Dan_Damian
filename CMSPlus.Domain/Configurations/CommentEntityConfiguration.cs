b9f3f2c9b3197cffe6e16a1b0afb477e991cef22
Damian Dan <dan.damian@isa.utm.md>
2025-01-22 23:42:44 +0200

    Task2: Use migrations to make the necessary database changes to support having multiple comments for each topic.
    -Created the comment entity
    -Added reference to the Topic
    -Created the CommentEntityConfiguration
    -Updated database to apply the configuration
    -Created the migrations

---
+++ CMSPlus.Domain/Configurations/CommentEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using CMSPlus.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMSPlus.Domain.Configurations
+{
+    internal class CommentEntityConfiguration : IEntityTypeConfiguration<CommentEntity>
+    {
+        public void Configure(EntityTypeBuilder<CommentEntity> builder)
+        {
+            builder.ToTable("Comments");
+            builder.Property(x => x.Text).IsRequired();
+            builder.HasOne(x => x.Topic)
+                .WithMany(x => x.Comments)
+                .HasForeignKey(x => x.TopicId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ CMSPlus.Domain/Entities/CommentEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace CMSPlus.Domain.Entities
+{
+    public class CommentEntity : BaseEntity
+    {
+        public string Text { get; set; }
+
+        public int TopicId { get; set; }
+        public TopicEntity Topic { get; set; }
+    }
+}
